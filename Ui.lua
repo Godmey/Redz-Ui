6382c80f41b1bbe4cecebb3de03a1095a4c6b3eb
Godmey <166684533+Godmey@users.noreply.github.com>
2024-09-23 17:37:17 +0700

    Update Ui.lua

--- Ui.lua
+++ Ui.lua
@@ -52,8 +52,8 @@ local redzlib = {
 				ColorSequenceKeypoint.new(0.50, Color3.fromRGB(32.5, 32.5, 32.5)),
 				ColorSequenceKeypoint.new(1.00, Color3.fromRGB(27.5, 25, 30))
 			}),
-			["Color Hub 2"] = Color3.fromRGB(0,0,0),
-			["Color Stroke"] = Color3.fromRGB(5, 5, 5),
+			["Color Hub 2"] = Color3.fromRGB(30,30,30),
+			["Color Stroke"] = Color3.fromRGB(0, 0, 0),
 			["Color Theme"] = Color3.fromRGB(127, 0, 255),
 			["Color Text"] = Color3.fromRGB(255, 255, 255),
 			["Color Dark Text"] = Color3.fromRGB(255, 255, 255)
